b71b2836261c26ba7c212ae8ab24e463c29de19f
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-11-22 12:04:56 +0300

    new file rules

--- src/responder/web/helper.py
+++ src/responder/web/helper.py
@@ -2,10 +2,14 @@ import os
 
 from flask import request
 
+from responder.constants import *
+
 
 def get_project_name() -> str:
     return os.environ.get('TEST_PROJECT') or request.host
 
 
 def can_serve(file: dict) -> bool:
-    return file['name'].endswith(('.html', '.css')) and file['size'] <= 1024000
+    file_ext = file['name'].split('.')[-1].lower()
+
+    return file_ext in FILE_SERVE_RULES and file['size'] <= FILE_SERVE_RULES[file_ext]
