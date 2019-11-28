b4777ca4d437066448f5f7e3b0058fb98fe9c7de
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-11-28 17:31:23 +0300

    relatiıon update between projects <-> files

--- src/responder/db.py
+++ src/responder/db.py
@@ -14,6 +14,6 @@ def get_project(project_name: str) -> Optional[dict]:
 
 
 def get_file(project: dict, path: str) -> Optional[dict]:
-    filters = {'radiksType': 'file', 'project': project['_id'], 'username': project['username'],
+    filters = {'radiksType': 'file', 'project': project['_id'], 'signingKeyId': project['signingKeyId'],
                'tag': project['tag'], 'fullPath': path, 'deleted': False}
     return db['radiks-server-data'].find_one(filters, sort=[('createdAt', pymongo.DESCENDING)])
